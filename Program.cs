390680eefbd2f0ad47d849df8d44dc752f5062c0
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-07 11:02:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Create ConcreteCreatorA and ConcreteCreatorB instances
+        Creator creatorA = new ConcreteCreatorA();
+        Creator creatorB = new ConcreteCreatorB();
+
+        // Use the FactoryMethod to create products
+        IProduct productA = creatorA.FactoryMethod();
+        IProduct productB = creatorB.FactoryMethod();
+
+        // Display the products
+        productA.Display();
+        productB.Display();
+    }
+}
+
+public interface IProduct
+{
+    void Display();
+}
+
+public class ConcreteProductA : IProduct
+{
+    public void Display()
+    {
+        Console.WriteLine("Concrete Product A");
+    }
+}
+
+public class ConcreteProductB : IProduct
+{
+    public void Display()
+    {
+        Console.WriteLine("Concrete Product B");
+    }
+}
+
+public abstract class Creator
+{
+    public abstract IProduct FactoryMethod();
+}
+
+public class ConcreteCreatorA : Creator
+{
+    public override IProduct FactoryMethod()
+    {
+        return new ConcreteProductA();
+    }
+}
+
+public class ConcreteCreatorB : Creator
+{
+    public override IProduct FactoryMethod()
+    {
+        return new ConcreteProductB();
+    }
+}
